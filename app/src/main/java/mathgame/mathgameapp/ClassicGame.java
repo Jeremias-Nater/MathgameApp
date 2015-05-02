c40b96b0794aebc265145a09ea5b234d8b5a19af
Jeremias Nater <jeremias.nater@casasoft.ch>
2015-05-02 16:57:58 +0200

    Added Second View
    https://developer.android.com/training/basics/firstapp/starting-activity
    .html
    
    Step 2

--- app/src/main/java/mathgame/mathgameapp/ClassicGame.java
+++ app/src/main/java/mathgame/mathgameapp/ClassicGame.java
@@ -1,36 +1,23 @@
 package mathgame.mathgameapp;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
 
-public class mathgame_main extends ActionBarActivity {
-
-    public final static String EXTRA_MESSAGE = "mathgameapp.MESSAGE";
+public class ClassicGame extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mathgame_main);
-    }
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, ClassicGame.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        setContentView(R.layout.activity_classic_game);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_mathgame_main, menu);
+        getMenuInflater().inflate(R.menu.menu_classic_game, menu);
         return true;
     }
 
